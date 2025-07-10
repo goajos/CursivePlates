92a12820bc96e046f2704cc257c808a18ce43325
goajos <jappecremers@gmail.com>
2025-07-10 14:08:33 +0200

    efficiency updates by using curseData over global env from ShaguPlates

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ Addon for combining Cursives DoT tracking and ShaguPlates nameplates.
 
 # Dependencies
 For this addon to work you need to install both addons [Cursive](https://github.com/pepopo978/Cursive) and [ShaguPlates](https://github.com/shagu/ShaguPlates) and also the client mod [SuperWoW](https://github.com/balakethelock/SuperWoW).
+
+# Information
+For debuffs to show up you need to tick "Enable debuffs" in the ShaguPlates settings. Only the debuffs coming in from Cursive will have timers. To not show other players debuffs tick "Only Show Own Debuffs (Experimental)" in the ShaguPlates settings.
--- libcursive.lua
+++ libcursive.lua
@@ -10,47 +10,12 @@ local scanner = libtipscan:GetScanner("libcursive")
 
 libcursive.objects = {}
 
-function libcursive:GetDuration(effect, rank)
-  if L["debuffs"][effect] then
-    local rank = rank and tonumber((string.gsub(rank, RANK, ""))) or 0
-    local rank = L["debuffs"][effect][rank] and rank or libcursive:GetMaxRank(effect)
-    local duration = L["debuffs"][effect][rank]
-
-    if effect == L["dyndebuffs"]["Rupture"] then
-      -- Rupture: +2 sec per combo point
-      duration = duration + GetComboPoints()*2
-    elseif effect == L["dyndebuffs"]["Kidney Shot"] then
-      -- Kidney Shot: +1 sec per combo point
-      duration = duration + GetComboPoints()*1
-    elseif effect == L["dyndebuffs"]["Demoralizing Shout"] then
-      -- Booming Voice: 10% per talent
-      local _,_,_,_,count = GetTalentInfo(2,1)
-      if count and count > 0 then duration = duration + ( duration / 100 * (count*10)) end
-    elseif effect == L["dyndebuffs"]["Shadow Word: Pain"] then
-      -- Improved Shadow Word: Pain: +3s per talent
-      local _,_,_,_,count = GetTalentInfo(3,4)
-      if count and count > 0 then duration = duration + count * 3 end
-    elseif effect == L["dyndebuffs"]["Frostbolt"] then
-      -- Permafrost: +1s per talent
-      local _,_,_,_,count = GetTalentInfo(3,7)
-      if count and count > 0 then duration = duration + count end
-    elseif effect == L["dyndebuffs"]["Gouge"] then
-      -- Improved Gouge: +.5s per talent
-      local _,_,_,_,count = GetTalentInfo(2,1)
-      if count and count > 0 then duration = duration + (count*.5) end
-    end
+function libcursive:GetDuration(effect, _rank)
+  curseData = Cursive.curses:GetCurseData(effect)
+  if curseData then
+    duration = curseData.duration
     return duration
-  else
-    return 0
   end
-end
-
-function libcursive:GetMaxRank(effect)
-  local max = 0
-  for id in pairs(L["debuffs"][effect]) do
-    if id > max then max = id end
-  end
-  return max
 end
 
 function libcursive:UpdateDuration(unit, unitlevel, effect, duration)
@@ -67,23 +32,18 @@ function libcursive:UpdateUnits()
   ShaguPlates.uf:RefreshUnit(ShaguPlates.uf.target, "aura")
 end
 
-function libcursive:AddEffect(unit, unitlevel, effect, duration, caster, event)
+function libcursive:AddEffect(unit, unitlevel, effect, duration, caster)
   if not unit or not effect then return end
   unitlevel = unitlevel or 0
   if not libcursive.objects[unit] then libcursive.objects[unit] = {} end
   if not libcursive.objects[unit][unitlevel] then libcursive.objects[unit][unitlevel] = {} end
   if not libcursive.objects[unit][unitlevel][effect] then libcursive.objects[unit][unitlevel][effect] = {} end
 
-  -- save current effect as lastspell
-  lastspell = libcursive.objects[unit][unitlevel][effect]
-
   libcursive.objects[unit][unitlevel][effect].effect = effect
   libcursive.objects[unit][unitlevel][effect].start_old = libcursive.objects[unit][unitlevel][effect].start
   libcursive.objects[unit][unitlevel][effect].start = GetTime()
   libcursive.objects[unit][unitlevel][effect].duration = duration
   libcursive.objects[unit][unitlevel][effect].caster = caster
-  libcursive.objects[unit][unitlevel][effect].event = event
-
 
   libcursive:UpdateUnits()
 end
@@ -113,8 +73,7 @@ function libcursive:UnitDebuff(unit, id)
         duration = curseData.duration
         timeleft = Cursive.curses:TimeRemaining(curseData)
       else
-        duration = libcursive.objects[unitname][unitlevel][effect].duration
-        timeleft = duration + libcursive.objects[unitname][unitlevel][effect].start - GetTime()
+        return
       end
       caster = libcursive.objects[unitname][unitlevel][effect].caster
     end
@@ -131,8 +90,7 @@ function libcursive:UnitDebuff(unit, id)
         duration = curseData.duration
         timeleft = Cursive.curses:TimeRemaining(curseData)
       else
-        duration = libcursive.objects[unitname][0][effect].duration
-        timeleft = duration + libcursive.objects[unitname][0][effect].start - GetTime()
+        return
       end
       caster = libcursive.objects[unitname][0][effect].caster
     end
